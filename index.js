0d8dd5cac0855a8492d65d113e2fb17132e75ec0
Matt <smvgggsdd@gmail.com>
2022-01-25 15:22:23 -0600

    Added preliminary slash commands

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 {/* <script type ='text/javascript' src='config.js' /> */}
-const Config = require('./config');
+const { token } = require('./config');
 
 const { Client, Intents } = require('discord.js');
+
+const prefix = '-';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
 
@@ -9,5 +11,19 @@ client.once('ready', () => {
     console.log('Bobby is online');
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if (commandName === 'user') {
+        await interaction.reply('User info.');
+    }
+});
+
 // This needs to be last line of file
-client.login(Config.config.MY_KEY);
+client.login(token);
