33cf9043de4f63140b07067bb3a89cbf6034fb42
Matt <smvgggsdd@gmail.com>
2022-01-26 16:09:10 -0600

    Refactor event handling

---
+++ events/interactionCreate.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+
+        if (!interaction.isCommand()) return;
+
+        // need to use interaction.client to access client instance in this file
+
+        const command = interaction.client.commands.get(interaction.commandName);
+        if (!command) return;
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
+        }
+    },
+};
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
-const { token } = require('./config');
+const { token } = require('./config.json');
 const { Client, Collection, Intents } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
 
 client.commands = new Collection();
+
+// Search through subdirectories to find commands/ events
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
@@ -14,24 +18,37 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-    console.log('Bobby is online');
-});
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-});
+}
+
+
+//might not need next block
+// client.once('ready', () => {
+//     console.log('Bobby is online');
+// });
+
+// or this
+// client.on('interactionCreate', async interaction => {
+//     if (!interaction.isCommand()) return;
+
+//     const command = client.commands.get(interaction.commandName);
+
+//     if (!command) return;
+
+//     try {
+//         await command.execute(interaction);
+//     } catch (error) {
+//         console.error(error);
+//         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
+//     }
+// });
+
 
 // This needs to be last line of file
 client.login(token);
